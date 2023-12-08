13b01ea2ad08a5df911d4ab67d8b765445c920f7
analiviaexe <analivia.exe@gmail.com>
2023-12-08 00:03:36 -0300

    adicionando structs e ajustando init

--- ping-pong-os/ppos_disk.c
+++ ping-pong-os/ppos_disk.c
@@ -1,66 +1,97 @@
 /*esse arquivo n deveria ser dado?*/
 #include "ppos_disk.h"
+#include "disk.h"
+#include "ppos-core-globals.h"
 #include "ppos.h"
+#include <limits.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// PROJETO B
 
-//PROJETO B
+disk_t *disco;
+FilaPedidos *fila_pedidos;
+struct sigaction action;
 
-static disk_t disk ;// hard disk structure
-task_t taskManager;
+void capturaSinal(int signum) {
+  if (disco->state == 0) {
+    task_resume(disco->tarefa_gerenciadora);
+    disco->state = 1;
+  }
+  disco->sinal = disco->state;
+}
 
 // interface de gerencia do disco
-void diskManager(){
-/*IMPLEMENTAR*/
+void diskManager() { /*IMPLEMENTAR*/
 }
 
 // inicializacao do gerente de disco
-int disk_mgr_init (int *numBlocks, int *blockSize) {
-    
-    int initDisk = disk_cmd(DISK_CMD_BLOCKSIZE, 0, 0);
+int disk_mgr_init(int *numBlocos, int *tamBloco) {
 
-    if (!initDisk)
-    {
-        printf("Erro ao inicializar gerente de disco");
-        return -1;
-    }
-    
-    //inicializando tamanho de blocos e numero de blocos
-    *blockSize = disk_cmd (DISK_CMD_BLOCKSIZE, 0, 0);
-    *numBlocks = disk_cmd (DISK_CMD_DISKSIZE, 0, 0);
+  int inicializaDisco = disk_cmd(DISK_CMD_BLOCKSIZE, 0, 0);
+  if (!inicializaDisco) {
+    printf("Erro ao inicializar gerente de disco");
+    return -1;
+  }
 
-    if (*numBlocks == NULL || *blockSize == NULL )
-    {
-        printf("Erro ao inicializar gerente de disco");
-        return -1;
-    }
-    
+  // inicializando tamanho de blocos e numero de blocos
+  *tamBloco = disk_cmd(DISK_CMD_BLOCKSIZE, 0, 0);
+  *numBlocos = disk_cmd(DISK_CMD_DISKSIZE, 0, 0);
 
-      // tarefa gerenciadora do disco
-    task_create(&taskManager,diskManager,"Disk Manager");
+  if (*numBlocos == NULL || *tamBloco == NULL) {
+    printf("Erro ao inicializar gerente de disco");
+    return -1;
+  }
 
-    return 0;
+  // inicializa disco
+  disco = (disk_t *)malloc(sizeof(disk_t));
+  disco->sem_disco = (semaphore_t *)malloc(sizeof(semaphore_t));
+  disco->tarefas_supensas = (semaphore_t *)malloc(sizeof(semaphore_t));
+  disco->state = 0; // sem uso
+  // sem_create para tarefas_supensas
+
+  // inicializa tarefa gerenciadora
+  disco->tarefa_gerenciadora = (disk_t *)malloc(sizeof(disk_t));
+  disco->tarefa_gerenciadora->tipo_task = 0; // setando como tarefa de sistema
+  disco->tarefa_gerenciadora->next = disco->tarefa_gerenciadora->prev = NULL;
+  task_create(&disco->tarefa_gerenciadora, diskManager,
+              "Gerenciadora de disco");
+
+  // inicializa fila de pedidos
+  fila_pedidos = (FilaPedidos *)malloc(sizeof(FilaPedidos));
+  fila_pedidos->head = NULL;
+  fila_pedidos->tail = NULL;
+
+  // inicializa sinal
+  action.sa_handler = capturaSinal;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  sigaction(SIGUSR1, &action, 0);
+
+  return 0;
 }
 
-/*Cada tarefa que solicita uma operação de leitura/escrita no disco deve ser suspensa
-até que a operação solicitada seja completada.*/
+/*Cada tarefa que solicita uma operação de leitura/escrita no disco deve ser
+suspensa até que a operação solicitada seja completada.*/
 
 // leitura de um bloco, do disco para o buffer
-int disk_block_read (int block, void *buffer) {
-    
+int disk_block_read(int bloco, void *buffer) {
 
-    if (block < 0 || block >= disk->numBlocks) {
-        printf("Erro: Bloco inválido.\n");
-        return -1;
-    }
-    return 0;
+  if (bloco < 0 || bloco >= fila_pedidos->blocos_percorridos) {
+    printf("Erro: Bloco inválido.\n");
+    return -1;
+  }
+  return 0;
 }
 
 // escrita de um bloco, do buffer para o disco
-int disk_block_write (int block, void *buffer) {
-        if (block < 0 || block >= disk->numBlocks) {
-        printf("Erro: Bloco inválido.\n");
-        return -1;
-    }
+int disk_block_write(int bloco, void *buffer) {
+  if (bloco < 0 || bloco >= fila_pedidos->blocos_percorridos) {
+    printf("Erro: Bloco inválido.\n");
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
--- ping-pong-os/ppos_disk.h
+++ ping-pong-os/ppos_disk.h
@@ -12,25 +12,40 @@
 // tipicamente um disco rigido.
 
 // estrutura que representa um disco no sistema operacional
-typedef struct
-{
+typedef struct disk_t {
+  struct task_t *tarefas_supensas;    // tarefas suspensas do disco
+  struct task_t *tarefa_atual;        // tarefa que esta sendo atendida
+  struct task_t *tarefa_gerenciadora; // tarefa gerenciadora de disco
+  int state; // indica estado do disco, 0 para livre, 1 em utilizacao
+  int sinal; // flag sinal de disco
+  semaphore_t sem_disco;
   // completar com os campos necessarios
-  int numBlocks; //256
-  int blockSize; //64
-  int block;
-  void *buffer;
-} disk_t ;
+} disk_t;
+
+typedef struct Pedido {
+  task_t *tarefa;
+  void *buffer; // Ponteiro genérico para o buffer
+  int bloco;    // Valor do bloco
+  int pedido;   // Valor do pedido
+} Pedido;
+
+typedef struct FilaPedidos {
+  struct Pedido *head;    // Ponteiro para o primeiro nó da fila
+  struct Pedido *tail;    // Ponteiro para o último nó da fila
+  int headCod;            // indica onde esta a cabeça do leitor
+  int blocos_percorridos; // blocos percorridos pelo leitor de disco
+} FilaPedidos;
 
 // inicializacao do gerente de disco
 // retorna -1 em erro ou 0 em sucesso
 // numBlocks: tamanho do disco, em blocos
 // blockSize: tamanho de cada bloco do disco, em bytes
-int disk_mgr_init (int *numBlocks, int *blockSize) ;
+int disk_mgr_init(int *numBlocks, int *blockSize);
 
 // leitura de um bloco, do disco para o buffer
-int disk_block_read (int block, void *buffer) ;
+int disk_block_read(int block, void *buffer);
 
 // escrita de um bloco, do buffer para o disco
-int disk_block_write (int block, void *buffer) ;
+int disk_block_write(int block, void *buffer);
 
 #endif
